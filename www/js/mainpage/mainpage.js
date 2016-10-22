bb03e7d4622fdc2de36b42eb162b2aa07c4e0921
Siddiq Akbar <siddiq@gmail.com>
2016-10-22 15:10:32 +0400

    wip

--- www/js/mainpage/mainpage.js
+++ www/js/mainpage/mainpage.js
@@ -2,6 +2,7 @@
 angular.module('starter.controllers')
 .controller('MainPageCtrl', function($scope) {
     $scope.vm = {
+        doc: "",
         url: "..."
     };
 
@@ -34,12 +35,51 @@ angular.module('starter.controllers')
     };
 
     $scope.openfile = function () {
+        $scope.vm.doc = ".";
+
         // openfile
         window.FilePicker.pickFile(function (url) {
+            // upload now
+            $scope.vm.doc = url;
+            $scope.upload(url);
+
             navigator.notifications.alert(url);
         }, function (error) {
             navigator.notifications.alert(error);
         })
 
     }
+
+    $scope.upload = function (fileURL) {
+
+        // !! Assumes variable fileURL contains a valid URL to a text file on the device,
+        //    for example, cdvfile://localhost/persistent/path/to/file.txt
+
+        var win = function (r) {
+            console.log("Code = " + r.responseCode);
+            console.log("Response = " + r.response);
+            console.log("Sent = " + r.bytesSent);
+        }
+
+        var fail = function (error) {
+            alert("An error has occurred: Code = " + error.code);
+            console.log("upload error source " + error.source);
+            console.log("upload error target " + error.target);
+        }
+
+        var options = new FileUploadOptions();
+        options.fileKey = "file";
+//        options.fileName = fileURL.substr(fileURL.lastIndexOf('/') + 1);
+        options.fileName = fileURL;
+        options.mimeType = "text/plain";
+
+        var params = {};
+        params.value1 = "test";
+        params.value2 = "param";
+
+        options.params = params;
+
+        var ft = new FileTransfer();
+        ft.upload(fileURL, encodeURI("http://puliyal.com/printman/uploads"), win, fail, options);
+    };
 })
